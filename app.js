846d99765da2c4c10a962acf335bd372ca60b306
mosubborini2000 <mosubborini2000@gmail.com>
2023-06-06 18:34:28 +0000

    lab06

--- app.js
+++ app.js
@@ -1,21 +1,55 @@
-const fname=prompt("enter your name:");
+'use strict'
+
+
+const fname = prompt("enter your name:");
+
 console.log(fname);
 const gender = prompt("Enter your gender (male or female):");
 console.log(gender);
 let age = parseInt(prompt("Enter your age:"));
 console.log(age);
-if(age<=0){alert("please enter a correct age")}
+if (age <= 0) { alert("please enter a correct age") }
 
-let m=confirm("do you want to skip welcoming message?");
-if(m==false){
-    if(gender=="male")
-    {alert("welcome mr."+fname)}
-    else if(gender=="female"){alert("welcome ms."+fname)}
-    else alert("welcome "+fname);
-
-
+let m = confirm("do you want to skip welcoming message?");
+if (m == false) {
+    if (gender == "male") { alert("welcome mr." + fname) }
+    else if (gender == "female") { alert("welcome ms." + fname) }
+    else alert("welcome " + fname);
 }
 
+function questionAnswer() {
+    const questionsArray = ["Are you good? (Yes or No)",
+     "Do you like boxing? (Yes or No)",
+      "Did you finish your study? (Yes or No)"];
+    let answers = [];
+
+    for (let i = 0; i < questionsArray.length; i++) {
+        let answer = prompt(questionsArray[i]);
+        if(answer==null){ alert("invalidAnswer!")
+    }
+    if (answer !== "Yes" && answer !== "No") {
+        alert("Invalid answer!");
+    
+      }
+        answers.push(answer);
+    }
+    
+    return answers;
+}
+function printAnswers() {
+    for (let i = 0; i < answersQuestion.length; i++) {
+        const printAnswers = answersQuestion[i];
+        console.log(printAnswers);
+        
+    }
+}
+
+let answersQuestion = questionAnswer();
+console.log(answersQuestion);
+printAnswers();
 
 
 
+
+
+    
